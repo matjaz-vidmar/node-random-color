4a05c4af3e083980c0f17299fcde2deb9d35325a
Matjaz Vidmar <wei.majiang@gmail.com>
2022-09-07 14:53:20 +0200

    Draft project

---
+++ index.js
@@ -0,0 +1,26 @@
+import chalk from 'chalk';
+
+import randomColor  from('randomcolor')
+const color = randomColor()
+// function square(num) {
+// const squareArray = [];
+// let result;
+// for (let i = 0; i < num; i++) {
+// result = '';
+// for (let j = 0; j < num; j++) {
+// result += '#';
+// }
+// squareArray.push(result);
+// }
+// return squareArray.join('\n');
+// }
+// console.log(square(20));
+
+// we could just type out the #
+// with kuler you get all the lines
+// randomColor
+// search node command line arguments
+// console.log(process.argv[2])
+//
+//
+//
